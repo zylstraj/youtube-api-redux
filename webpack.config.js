ed93088fb4586ab2a489bfa574f7b86080fb5287
Josh Zylstra <zylstrajosh@gmail.com>
2017-01-16 09:41:15 -0800

    setting up environment

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'app') + '/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/assets'
+  },
+  module: {
+    loaders: [
+    {
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'app'),
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    },
+    {
+      test: /\.css$/,
+      loaders:[
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]'
+          ],
+    }
+    ]
+  },
+  devServer: {
+    stats: 'errors-only'
+  }
+
+
+};
